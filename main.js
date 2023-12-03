391ba9e1327c9de19f43f754cfe9bedc58ff7c8d
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-12-03 16:22:15 -0300

    acessei o estado no módulo main.js, com o import

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+import { getJoao, getMaria } from "./joaoEMaria.js";
+
 console.log('módulo main');
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maria tem ', getMaria(), ' maçãs!');
