33a57ed192427feadfbd172a435def49483bb6dc
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2023-12-03 16:09:56 -0300

    concertei o documento index.html.spec.js

--- e2e/index.html.spec.js
+++ e2e/index.html.spec.js
@@ -55,10 +55,10 @@ test.describe("index.html", () => {
     );
   });
 
-  test("Deve ter um script módulo na cabeça do documento para src/main.js.", async ({ page }) => {
+  test("Deve ter um script módulo na cabeça do documento para main.js.", async ({ page }) => {
     await page.goto(`http://localhost:5500`);
     const script = page.locator("head > script");
-    await expect(script).toHaveAttribute('src', 'src/main.js');
+    await expect(script).toHaveAttribute('src', 'main.js');
     await expect(script).toHaveAttribute('type', 'module');
   });
 });
